2f0ce357806cc7207daa5d1fc79453ae6fb4fea1
vermahoney1 <119039108+vermahoney1@users.noreply.github.com>
2022-12-01 23:33:24 +0530

    Create ifkeyboard.py

---
+++ ifkeyboard.py
@@ -0,0 +1,12 @@
+x="ankit"
+if "a" in x:
+    print("a is present in x")
+else:
+    print(" a is not present in x")
+
+# check empty or not
+x="Ankit"
+if x: # true if string is notg empty
+    print("string is not empty")
+else:
+    print("string is empty")
