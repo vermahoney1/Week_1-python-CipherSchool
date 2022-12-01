c8f8626451ebed2976bc731b9d64268f05eaa9b2
vermahoney1 <119039108+vermahoney1@users.noreply.github.com>
2022-12-01 23:30:45 +0530

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,12 @@
+number=input("enter your number: ")
+#"1256"
+total=0
+i=0
+while i<len(number):
+    total+=int(number[i])
+    i+=1
+print(total)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
