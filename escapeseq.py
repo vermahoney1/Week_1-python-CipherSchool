fcacf4a8e91e8acddfaf598cc35e68ee0b2835d2
vermahoney1 <119039108+vermahoney1@users.noreply.github.com>
2022-12-01 23:23:42 +0530

    Create escapeseq.py

---
+++ escapeseq.py
@@ -0,0 +1,12 @@
+print("hello \"world\" python")
+print('I\'am Ankit')
+print("Line A\nLine B\n LIne c") # \n is for new line
+# \t is tab here
+print("name\tAnkit")
+print("this is backlash\\") #\\ is for backlash
+print("hell\bol")
+# this is comment
+#  escape character as a normal text
+# output: Line A \ Line B
+print("Line A \\ Line B")
+print(r"Line A \ Line B")
